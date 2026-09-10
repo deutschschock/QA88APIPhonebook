fae5d34312c7c7c4cc849a171c605bd4293d2076
deutschschock <deutschschock@gmail.com>
2026-09-10 19:46:21 +0200

    new tests user_registration

--- utils/conftest.py
+++ utils/conftest.py
@@ -2,21 +2,21 @@ import pytest
 import requests
 import time
 import random
-from config import *
+from utils.config import *
 from faker import Faker
 
 from models.user_dto import User
 
 fake = Faker()
 
-@pytest.fixture(scope="session"):
+@pytest.fixture(scope="session")
 def registration_url():
     return BASE_URL + API_VERSION + REGISTRATION_URL
 @pytest.fixture(scope="session")
 def login_url():
     return BASE_URL + API_VERSION + LOGIN_URL
 @pytest.fixture(scope="session")
-def session_id():
+def session():
     s = requests.Session()
     yield s  #передать данные в тест
     s.close()
@@ -24,5 +24,10 @@ def session_id():
 @pytest.fixture(scope="function")
 def random_user():
     username = f"qa_{int(time.time())}_{fake.email()}"
-    password = fake.password(length=random.randrange(8, 16) )
+    password = fake.password(length=random.randrange(8, 15),
+                             special_chars=False,
+                             digits = True,
+                             upper_case = True,
+                             lower_case=True
+                             )+'$'
     yield User(username=username, password=password)
